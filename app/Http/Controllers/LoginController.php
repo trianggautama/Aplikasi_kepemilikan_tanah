c12fd45c9bebf0d3819d79185f263dc717fae815
muhammadasadbastomi <tomycules@gmail.com>
2021-07-17 21:03:48 +0800

    tes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -18,6 +18,7 @@ class LoginController extends Controller
         $credentials = $request->validate([
             'username' => ['required'],
             'password' => ['required'],
+
         ]);
 
         if (Auth::attempt($credentials)) {
